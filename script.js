6878a188dee49760484e431c7151c2006a15a584
DSPMODAL <DSPMODAL@GMAIL.COM>
2026-02-10 13:07:28 +0530

    added new script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBIGoh9yZJ1r_VIkJZi0YI9gJkQ9DbTr60",
-  authDomain: "my-first-login-f35e7.firebaseapp.com",
-  projectId: "my-first-login-f35e7",
-  storageBucket: "my-first-login-f35e7.firebasestorage.app",
-  messagingSenderId: "903114259635",
-  appId: "1:903114259635:web:8d97ab4004f4786309c4c6",
-  measurementId: "G-41KRVQNLT8"
+  authDomain: "my-first-login.firebaseapp.com",
+  projectId: "my-first-login",
+  appId: "1:123:web:abc"
 };
